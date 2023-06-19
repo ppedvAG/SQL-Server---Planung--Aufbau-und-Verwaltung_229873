8c2404618c5137742ac82d15e6b2a4c8f5c413ac
Rauchendraser <andreasr@ppedv.de>
2023-06-19 15:45:04 +0200

    Server und DB Settings

---
+++ SQL_AfubauPlanungMonitoring/SQL_AfubauPlanungMonitoring/03_Serversetttings.sql
@@ -0,0 +1,31 @@
+/*
+
+RAM
+
+
+MIN 0
+ein eingestellter Wert z#hlt erst wenn belegt
+
+
+MAX
+gilt auf der Stelle
+Gesamter RAM -OS - evtl anere Instanzen od Software
+OS: mind 2 bis 10GB
+
+MAXDOP 
+von 2 auf 4 gesetzt weil 4 Kerne
+
+Pfade
+
+D:\_SQLDBDATA\.. passt . hauptsache nicht c: \Program Files.
+
+TempDB
+soviele Dateien wie Kerne max 8
+und alle müssen gleich groß sein!!!
+
+
+
+
+
+
+
---
+++ SQL_AfubauPlanungMonitoring/SQL_AfubauPlanungMonitoring/05_DBSettings.sql
@@ -0,0 +1,26 @@
+/*
+
+
+
+
+select * from sysprocesses where dbid = db_id('msdb')
+
+Wie groß ist eine DB per default
+
+mdf  8 MB    5MB   3 MB
+ldf  8 MB    2MB
+
+Wachstumrate: 64 MB    pro mdf und ldf  
+früher		  1 MB für MDF   10% bei Ldf
+
+
+unnötige HDD ZUgriffe vermeiden
+
+
+Groß genug machen:  wie groß wird die DB am Ende der Laufzeit ungefähr sein( Hardware)
+
+
+
+
+
+
